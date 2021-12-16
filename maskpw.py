8672d338af03a7497dcb1bcedf860e26c4371d45
jcgo <jeancgo@pm.me>
2021-12-16 13:28:02 -0400

    feat: update version number to 0.5.5

--- maskpw.py
+++ maskpw.py
@@ -1,6 +1,6 @@
 """A simple library to ask the user for a password. Similar to getpass.getpass() but allows to specify a default mask (like '*' instead of blank)."""
 
-__version__ = "0.5.0"
+__version__ = "0.5.5"
 
 from sys import platform, stdin
 
