dbf35e595b85fdc1a9432bfa4cf8d8072b978aca
jcgo <jeancgo@pm.me>
2021-12-16 13:11:51 -0400

    refactor: move `/maskpw/maskpw.py` to `/maskpw.py`

--- maskpw.py
+++ maskpw.py
@@ -1,3 +1,7 @@
+"""A simple library to ask the user for a password. Similar to getpass.getpass() but allows to specify a default mask (like '*' instead of blank)."""
+
+__version__ = "0.5.0"
+
 from sys import platform, stdin
 
 if platform == "win32":
